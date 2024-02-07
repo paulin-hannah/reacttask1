e7f9f93e14f35cb71b6c45c78788f4d003e4a2c0
paulin <vivin.rufus@gmail.com>
2024-02-07 23:16:24 +0530

    added data

---
+++ src/App.js
@@ -0,0 +1,227 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicroscope } from '@fortawesome/free-solid-svg-icons';
+import { faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCapsules } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
+import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
+          import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'; // 
+function App() {
+  return (
+    <div
+      className="admin">
+      
+      <div className='Searchbar'>
+        <div className='Rectanglelogo'>
+
+          <FontAwesomeIcon icon={faSearch} size="lg" />
+           <h1 className='Rectanglesear' >Search</h1>
+          
+</div>
+        
+
+         
+        
+        <div className='Ellipse'>
+          <FontAwesomeIcon icon={faBell} size='2x'
+           
+ 
+          />
+
+        </div>
+      </div>
+      <h1 className="SalesInformation">Sales Information</h1>
+     <h1 className ="MedicoSales">Medico Sales</h1>
+   
+    
+      
+  
+
+
+
+      <div className='Table'>
+        <div className='Group47733'>
+          <div className='minus-square'></div>
+          <h1 className='InvoiceID1'>InvoiceID</h1>
+          <h1 className='Date0'>Date</h1>
+          <h1 className='Customer1'>Customer</h1>
+          <h1 className='PayableAmount'>PayableAmount</h1>
+          <h1 className='PaidAmount'>PaidAmount</h1>
+          <h1 className='Due'>Due</h1>
+        </div>
+        <div className='Group47734'>
+          <div className='minus-1'></div>
+          <h1 className='AHGA1'>AHGA68</h1>
+          <h1 className='Date1'>23/09/2022</h1>
+          <h1 className='JacobMarcus1'>Jacob Marcus</h1>
+          <h1 className='A1001'>$100</h1>
+          <h1 className='B0001'>$000</h1>
+          <h1 className='C0001'>$000</h1> 
+        </div>
+      
+        <div className='Group47735'>
+          <div className='minus-2'></div>
+          <h1 className='AHGA2'>AHGA68</h1>
+          <h1 className='Date2'>23/09/2022</h1>
+          <h1 className='JacobMarcus2'>Jacob Marcus</h1>
+          <h1 className='A1002'>$100</h1>
+          <h1 className='B0002'>$000</h1>
+          <h1 className='C0002'>$000</h1> 
+        </div>  
+        <div className='Group47736'>
+          <div className='minus-3'></div>
+          <h1 className='AHGA3'>AHGA68</h1>
+          <h1 className='Date3'>23/09/2022</h1>
+          <h1 className='JacobMarcus3'>Jacob Marcus</h1>
+          <h1 className='A1003'>$100</h1>
+          <h1 className='B0003'>$000</h1>
+          <h1 className='C0003'>$000</h1> 
+        </div>
+        <div className='Group47737'>
+          <div className='minus-4'></div>
+          <h1 className='AHGA4'>AHGA68</h1>
+          <h1 className='Date4'>23/09/2022</h1>
+          <h1 className='JacobMarcus4'>Jacob Marcus</h1>
+          <h1 className='A1004'>$100</h1>
+          <h1 className='B0004'>$000</h1>
+          <h1 className='C0004'>$000</h1> 
+        </div>
+        <div className='Group47738'>
+          <div className='minus-5'></div>
+          <h1 className='AHGA5'>AHGA68</h1>
+          <h1 className='Date5'>23/09/2022</h1>
+          <h1 className='JacobMarcus5'>Jacob Marcus</h1>
+          <h1 className='A1005'>$100</h1>
+          <h1 className='B0005'>$000</h1>
+          <h1 className='C0005'>$000</h1> 
+        </div>
+        <div className='Group47739'>
+          <div className='minus-6'></div>
+          <h1 className='AHGA6'>AHGA68</h1>
+          <h1 className='Date6'>23/09/2022</h1>
+          <h1 className='JacobMarcus6'>Jacob Marcus</h1>
+          <h1 className='A1006'>$100</h1>
+          <h1 className='B0006'>$000</h1>
+          <h1 className='C0006'>$000</h1> 
+        </div>
+        <div className='Group47740'>
+          <div className='minus-7'></div>
+          <h1 className='AHGA7'>AHGA68</h1>
+          <h1 className='Date7'>23/09/2022</h1>
+          <h1 className='JacobMarcus7'>Jacob Marcus</h1>
+          <h1 className='A1007'>$100</h1>
+          <h1 className='B0007'>$000</h1>
+          <h1 className='C0007'>$000</h1> 
+        </div>
+                 <div className='Group47741'>
+          <h1 className='AHGA8'>AHGA68</h1>
+          <div className='minus-8'></div>
+          <h1 className='Date8'>23/09/2022</h1>
+          <h1 className='JacobMarcus8'>Jacob Marcus</h1>
+          <h1 className='A1008'>$100</h1>
+          <h1 className='B0008'>$000</h1>
+          <h1 className='C0008'>$000</h1> 
+        </div>   
+        <div className='Group47742'>
+          <div className='minus-9'></div>
+          <h1 className='AHGA9'>AHGA68</h1>
+          <h1 className='Date9'>23/09/2022</h1>
+          <h1 className='JacobMarcus9'>Jacob Marcus</h1>
+          <h1 className='A1009'>$100</h1>
+          <h1 className='B0009'>$000</h1>
+          <h1 className='C0009'>$000</h1> 
+        </div>
+      </div>
+      <div className="Sidebar">
+        
+        <div className='element-3'>
+         
+    <FontAwesomeIcon icon={faTachometerAlt} />
+
+
+ 
+         </div>
+        <h1 className='Dashboard'>Dashboard</h1>
+
+        <h1 className='LabTest'>Lab Test</h1>
+        <div className='element4'>
+         
+    
+<FontAwesomeIcon icon={faMicroscope} />
+
+ 
+         </div>
+
+        <h1 className='Appointment'>Appointment</h1>
+        <div className='element5'>
+      
+<FontAwesomeIcon icon={faCalendarAlt}  />
+
+        </div>
+        <h1 className='MedicineOrder' >Medicine Order</h1>
+        <div className='element6'>
+          <FontAwesomeIcon icon={faCapsules} />
+                </div>
+
+        <h1 className='Message'> Message</h1>
+        <div className='element7'>
+          <FontAwesomeIcon icon={faEnvelope} />
+                </div>
+
+        <h1 className='Payment'>Payment</h1>
+        <div className='element8'>
+          <FontAwesomeIcon icon={faCreditCard}/>
+                </div>
+        <h1 className='Settings'>Settings</h1>
+        <div className='element9'>
+          <FontAwesomeIcon icon={faCog} /> 
+        </div>
+
+
+      </div>
+      <div className = "TopHeader"> </div>
+      
+      <div className='Frame5'>
+        <div className="Group47732"> </div>
+        <div>
+         <h4 className='Customer'> Customer </h4>
+        <div className='Button1'>
+            <h4>Enter Costumer Name </h4></div>
+        </div>
+        <div>
+         <h4 className='InvoiceID'> Invoice ID </h4>
+        <div className='Button2'>
+          <h4>Invoice ID </h4>
+          </div>
+        </div>
+                <div>
+         <h4 className='StartDate'> Start Date </h4>
+        <div className='Button3'>
+
+          <h4>Start Date </h4>
+        </div>
+        </div>
+        <div>
+          
+         <h4 className='EndDate'> End Date </h4>
+        <div className='Button4'>
+         <h4>End Date </h4>
+        </div>
+        </div>
+        </div>
+ 
+      </div>
+    
+      
+  
+
+  )
+}
+
+export default App
+     
